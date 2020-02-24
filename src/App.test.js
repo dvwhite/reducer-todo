3fd2cea62e4e0114b6717e977e0953d8d5034596
David White <dwhite0627@gmail.com>
2020-02-23 23:15:50 -0500

    Add tests for the App component

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { render } from '@testing-library/react';
+import App from './App';
+
+// Enzyme imports
+Enzyme.configure({ adapter: new Adapter() });
+
+// Tests
+test('renders the App component without crashing', () => {
+  const enzymeWrapper = shallow(<App />);
+  expect(enzymeWrapper.find('App')).not.toBeNull();
+});
