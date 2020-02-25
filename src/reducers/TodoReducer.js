1f01eb9479ab1cddcf9b04ca6aaa59e3a40fdc54
David White <dwhite0627@gmail.com>
2020-02-24 21:33:04 -0500

    Add comments and ADD_TODO action to the reducer

--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { ADD_TODO } from './../actions/TodoActions';
 
-export const initialState = [{
-  item: 'Learn about reducers',
-  completed: false,
-  id: 3892987589
-}];
+// The initial reducer state
+export const initialState = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  },
+];
 
+/* 
+* @function: The main reducer for the Todo list application
+* @param {Object} state: The JS object containing the state
+* @param {Object} action: The JS object containing the action object,
+*                         which has a type, payload, and target
+* @returns {Array} representing the newest App state
+*/
 export const TodoReducer = (state, action) => {
   switch(action.type) {
-    
-
+    case ADD_TODO:
+      return [ ...state, { ...action.payload } ];
     default:
       return state;
   }
