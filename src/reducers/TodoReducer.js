989b88b0e8cfb9b04dda0ad6c2c990e88a03b624
David White <dwhite0627@gmail.com>
2020-02-24 20:52:59 -0500

    Add basics of the todo reducer for the todo form

---
+++ src/reducers/TodoReducer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+};
+
+export const MasterReducer = (state, action) => {
+  switch(action.type) {
+    
+
+    default:
+      return state;
+  }
+}
