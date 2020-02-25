7ae271bd44ef48618bc6fe4c0ebcf16b8d3f6425
David White <dwhite0627@gmail.com>
2020-02-25 07:07:30 -0500

    Add the Form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 1%;
+  width: 60%;
+
+  @media (max-width: 800px) {
+    width: 80%;
+  }
+
+  @media (max-width: 600px) {
+    width: 90%;
+  }
+`
+
+const Input = styled.div`
+  padding: 1%;
+  width: 100%;
+  font-size: 1rem;
+  border-radius: 5px;
+  outline: 0;
+  border: 0.75 px solid #A9A9A9;
+`
+
+const Footer = styled.div`
+  height: 2rem;
+  width: 100%;
+  border-radius: 5px;
+  background: whitesmoke;
+`
+
+const Form = (props) => {
+  const [input, setInput] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+    props.dispatch()
+  }
+
+  const onChange = event => {
+    event.preventDefault();
+    setInput(event.target.value)
+  }
+
+  return (
+    <FormWrapper onSubmit={handleSubmit}>
+      <Input 
+        type="text"
+        name="text"
+        data-testid="form-component-input"
+        onChange={handleChange}
+        placeholder='Enter a todo...'
+      />
+    </FormWrapper>
+  );
+}
+
+export default Form;
