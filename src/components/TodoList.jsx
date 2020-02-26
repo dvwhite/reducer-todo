3f7c09a8216f0183b2041a3d82d9782092344a05
David White <dwhite0627@gmail.com>
2020-02-25 22:30:27 -0500

    Add comments to the TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,16 @@
 import React, { useReducer } from 'react';
+
+// Reducer imports
 import { initialState, TodoReducer } from './../reducers/TodoReducer.js';
+
+// Component imports
 import Todo from './Todo';
+import Form from './Form';
+
+// Styled component imports
 import styled from 'styled-components';
 
+// Styled components
 const Todos = styled.div`
   background: white;
   border: 1px solid gray;
@@ -16,12 +24,12 @@ const Todos = styled.div`
 */
 const TodoList = () => {
   const [state, dispatch] = useReducer(TodoReducer, initialState);
-  
   return (
     <Todos>
+      <Form dispatch={dispatch} />
       {
         state.map(todo => (
-          <Todo data={todo} key={todo.id}/>
+          <Todo data={todo} key={todo.id} dispatch={dispatch} />
         ))
       }
     </Todos>
