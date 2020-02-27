3b3e6e0ff63f1d241fbffb45aec1c2b8727ee5a0
David White <dwhite0627@gmail.com>
2020-02-26 21:59:01 -0500

    Add the CLEAR_COMPLETED_TODOS action type

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,2 +1,3 @@
 export const ADD_TODO = 'ADD_TODO'; // Add a todo list item
 export const COMPLETE_TODO = 'COMPLETE_TODO'; // Complete a todo list item
+export const CLEAR_COMPLETED_TODOS = 'CLEAR_COMPLETED_TODOS'; // Clear all completed todos from the todo list
