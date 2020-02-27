592c83c196ed346ceee1dcab3866df4e9a6c6ee6
David White <dwhite0627@gmail.com>
2020-02-26 21:57:28 -0500

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Styled components imports
+import styled from 'styled-components';
+
+// Styled components
+const Footer = styled.div`
+  height: 2rem;
+  width: 100%;
+  border-radius: 5px;
+  background: whitesmoke;
+`
+
+export default Footer;
