a72ba4e6ba2a87d885e904b6f2dd65ca57ab2e3a
David White <dwhite0627@gmail.com>
2020-02-26 21:58:45 -0500

    Refactor state, add CLEAR_COMPLETED, and fix COMPLETE_TODO actions

--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { ADD_TODO, COMPLETE_TODO } from "./../constants/ActionTypes";
+import { ADD_TODO, COMPLETE_TODO, CLEAR_COMPLETED_TODOS } from "./../constants/ActionTypes";
 
 // The initial reducer state
-export const initialState = [
-  {
-    item: "Learn about reducers",
-    completed: false,
-    id: 3892987589
-  }
-];
+export const initialState = {
+  todos: [
+    {
+      item: "Learn about reducers",
+      completed: false,
+      id: 3892987589
+    }
+  ]
+};
 
 /*
  * @function: The main reducer for the Todo list application
@@ -20,15 +22,28 @@ export const initialState = [
 export const TodoReducer = (state, action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [
+      return {
         ...state,
-        { item: action.payload, completed: false, id: Date.now() }
-      ];
+        todos: [
+          ...state.todos,
+          { item: action.payload, completed: false, id: Date.now() }
+        ]
+      };
     case COMPLETE_TODO:
-      return [
-        ...state.filter(item => item.id !== action.payload.id),
-        { ...action.payload, completed: true }
-      ];
+      return {
+        ...state,
+        todos: [
+          ...state.todos.filter(item => item.id !== action.payload.id),
+          { ...action.payload, completed: !action.payload.completed }
+        ]
+      };
+    case CLEAR_COMPLETED_TODOS:
+      return {
+        ...state,
+        todos: [
+          ...state.todos.filter(item => !item.completed)
+        ]
+      }
     default:
       return state;
   }
