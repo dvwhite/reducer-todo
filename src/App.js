3c64b8c03c76ba0d66b8a5899b8b6d6b39903f90
David White <dwhite0627@gmail.com>
2020-02-26 22:14:16 -0500

    Update styles in App.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   background: whitesmoke;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 55%;
-    border: 1px solid darkgray;
-    margin: auto;
-    box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 1px,
-                rgba(0, 0, 0, 0.12) 0px 2px 2px, 
-                rgba(0, 0, 0, 0.12) 0px 4px 4px, 
-                rgba(0, 0, 0, 0.12) 0px 8px 8px, 
-                rgba(0, 0, 0, 0.12) 0px 16px 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 55%;
+  border: 1px solid darkgray;
+  margin: auto;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 1px,
+              rgba(0, 0, 0, 0.12) 0px 2px 2px, 
+              rgba(0, 0, 0, 0.12) 0px 4px 4px, 
+              rgba(0, 0, 0, 0.12) 0px 8px 8px, 
+              rgba(0, 0, 0, 0.12) 0px 16px 16px;
 
-    @media (max-width: 800px) {
-      width: 80%;
-    }
+  @media (max-width: 800px) {
+    width: 80%;
+  }
 
-    @media (max-width: 600px) {
-      width: 95%;
-    }
+  @media (max-width: 600px) {
+    width: 95%;
+  }
 `
 
 function App() {
